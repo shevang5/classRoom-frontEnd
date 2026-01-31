0d16293f5bbd0b8281aa5a86c33ff546fdb6b779
shevang5 <shevangpalgam@gmail.com>
2026-01-31 13:01:21 +0530

    implement show class detail

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SubjectsList from "./pages/subjects/SubjectsList";
 import SubjectCreate from "./pages/subjects/Create";
 import ClassesList from "./pages/classes/list";
 import ClassesCreate from "./pages/classes/create";
+import ClassesShow from "./pages/classes/show";
 
 function App() {
   return (
@@ -58,6 +59,7 @@ function App() {
                   name: "classes",
                   list: "/classes",
                   create: "/classes/create",
+                  show: "/classes/show/:id",
                   meta: {
                     label: "Classes",
                     icon: <GraduationCap />
@@ -78,6 +80,7 @@ function App() {
                   <Route path="classes" >
                     <Route index element={<ClassesList />} />
                     <Route path="create" element={<ClassesCreate />} />
+                    <Route path="show/:id" element={<ClassesShow />} />
                   </Route>
                 </Route>
               </Routes>
